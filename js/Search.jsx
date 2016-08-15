1fa88675a032d85eca18007289ca2b201e0a1253
rdcoder <readercoder@outlook.com>
2016-08-15 17:12:42 -0600

    step-4-lintOK

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,6 +1,18 @@
 const React = require('react')
+const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
+const Search = () => (
+  <div className='container'>
+    <div className='shows'>
+      {data.shows.map((show) => (
+        <ShowCard show={show} key={show.imdbID} />
+      ))}
+    </div>
+  </div>
+)
+
+/*
 const Search = () => (
   <div className='container'>
     {data.shows.map((show) => (
@@ -8,7 +20,7 @@ const Search = () => (
     ))}
   </div>
 )
-
+*/
 /*
 const Search = () => (
   <div>
