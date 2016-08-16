66aaaac1298f792b3362df41a5c170ce645f1144
rdcoder <readercoder@outlook.com>
2016-08-15 18:49:28 -0600

    step-6-state

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,22 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard show={show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input type='text' className='search-input' placeholder='Search' />
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => (
+            <ShowCard show={show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
