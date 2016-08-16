d353261e76afdc7bde4ec767e9b4c50ad740e84f
rdcoder <readercoder@outlook.com>
2016-08-15 19:07:09 -0600

    step-7-addstate

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,13 +2,27 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = React.createClass({
+class Search extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      searchTerm: 'this is the default searchTerm'
+    }
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+
+  handleSearchTermChange (event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render () {
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>svideo</h1>
-          <input type='text' className='search-input' placeholder='Search' />
+          <h1 className='brand'>svideo: {this.state.searchTerm}</h1>
+          <input type='text' className='search-input' placeholder='Search'
+            value={this.state.searchTerm} onChange={this.handleSearchTermChange} />
         </header>
         <div className='shows'>
           {data.shows.map((show) => (
@@ -18,6 +32,6 @@ const Search = React.createClass({
       </div>
     )
   }
-})
+}
 
 module.exports = Search
