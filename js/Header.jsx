ba1c1ea5e590b8146b1708f42cf7de306f664e0f
rdcoder <readercoder@outlook.com>
2016-08-17 11:13:03 -0600

    step-14-headerdone

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,8 +1,31 @@
 const React = require('react')
 const { Link } = require('react-router')
+const { func, bool, string } = React.PropTypes
 
 const Header = React.createClass({
+  propTypes: {
+    handleSearchTermChange: func,
+    showSearch: bool,
+    searchTerm: string
+  },
+
+  handleSearchTermEvent (e) {
+    this.props.handleSearchTermChange(e.target.value)
+  },
+
   render () {
+    let utilSpace
+    if (this.props.showSearch) {
+      utilSpace = <input type='text' className='search-input' placeholder='Search' value={this.props.searchTerm} onChange={this.handleSearchTermEvent} />
+    } else {
+      utilSpace = (
+        <h2 className='header-back'>
+          <Link to='/search'>
+            Back
+          </Link>
+        </h2>
+      )
+    }
     return (
       <header className='header'>
         <h1 className='brand'>
@@ -10,11 +33,7 @@ const Header = React.createClass({
             svideo
           </Link>
         </h1>
-        <h2 className="header-back">
-          <Link to='/search'>
-            Back
-          </Link>
-        </h2>
+        {utilSpace}
       </header>
     )
   }
