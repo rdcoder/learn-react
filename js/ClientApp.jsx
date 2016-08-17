8577ca8c8372d812275f18f24a02b4b33003ab75
rdcoder <readercoder@outlook.com>
2016-08-17 10:07:05 -0600

    step-11-details

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -7,14 +7,31 @@ const Search = require('./Search')
 const Details = require('./Details')
 const { shows } = require('../public/data')
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout} >
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} shows={shows} />
-      <Route path='/details' component={Details} />
-    </Route>
-  </Router>
-)
+const App = React.createClass({
+
+  assignShow (nextState, replace) {
+    const showArray = shows.filter((show) => show.imdbID === nextState.params.id)
+    console.log('nextState', nextState)
+    console.log('showArray', showArray)
+    if (showArray.length < 1) {
+      // no show and send it back to home page
+      return replace('/')
+    }
+    Object.assign(nextState.params, showArray[0])
+    return nextState
+  },
+
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout} >
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} shows={shows} />
+          <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+        </Route>
+      </Router>
+    )
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- js/Details.jsx
+++ js/Details.jsx
@@ -3,8 +3,8 @@ const React = require('react')
 class Details extends React.Component {
   render () {
     return (
-      <div className='container'>
-        <pre style={{textAlign: 'left'}}><code>
+      <div style={{textAlign: 'left'}} className='container'>
+        <pre><code>
           {JSON.stringify(this.props.params, null, 4)}
         </code></pre>
       </div>
