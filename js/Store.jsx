790c0692ff1044ee4f993f38fbca57d9e9e4b88b
rdcoder <readercoder@outlook.com>
2016-08-17 15:13:50 -0600

    step-17-reduxdevtools

--- js/Store.jsx
+++ js/Store.jsx
@@ -18,7 +18,11 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = redux.createStore(reducer)
+// TODO: fem-23 add redux devtools here
+// const store = redux.createStore(reducer)
+const store = redux.createStore(reducer, initialState, redux.compose(
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f
+))
 
 // state to props
 const mapStateToProps = (state) => ({ searchTerm: state.searchTerm })
